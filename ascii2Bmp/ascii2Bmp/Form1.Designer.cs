ebbb8521fa10f71a00112113618857f41f562144
Chi Yeung <yeun0007@umn.edu>
2021-08-30 18:13:36 -0500

    source code from backup

---
+++ ascii2Bmp/ascii2Bmp/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ascii2Bmp
+{
+    partial class Ascii2Bmp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtOpen = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtOpen
+            // 
+            this.txtOpen.Location = new System.Drawing.Point(88, 23);
+            this.txtOpen.Name = "txtOpen";
+            this.txtOpen.Size = new System.Drawing.Size(412, 20);
+            this.txtOpen.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnOpen);
+            this.groupBox1.Controls.Add(this.txtOpen);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(518, 98);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Directory (raw)";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(7, 20);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // Ascii2Bmp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 445);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Ascii2Bmp";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox txtOpen;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnOpen;
+    }
+}
+
---
+++ ascii2Bmp/ascii2Bmp/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ascii2Bmp
+{
+    public partial class Ascii2Bmp : Form
+    {
+        public Ascii2Bmp()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            string path = "c:\\testimage\\";
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+            FileInfo[] fileInfo = directoryInfo.GetFiles();
+
+            if (null != fileInfo && fileInfo.Count() > 0)
+            {
+                //if(true==createHistogram(fileInfo))
+                    createBmp(fileInfo);
+            }
+        }
+
+        protected bool createHistogram(FileInfo[] fileInfo)
+        {
+            try
+            {
+                int[] histogram = new int[4096];
+
+                // build a histogram
+                for (int x = 0; x < fileInfo.Count(); x++)
+                {
+                    // convert 1 line of ascii to bytes
+                    string[] pixels = loadAscii(fileInfo[x]);
+                    for (int y = 0; y < pixels.Length; y++)
+                    {
+                        int value = int.Parse(pixels[y]);
+                        histogram[value]++;
+                    }
+                }
+
+                // get statistics
+                int min=4096;
+                int max=0;
+                int numOfShades=0;
+                for (int i=0; i<histogram.Length; i++)
+                {
+                    if (histogram[i] > 0)
+                    {
+                        numOfShades++;
+
+                        if (i < min)
+                            min = i;
+
+                        if (i > max)
+                            max = i;
+                    }
+
+                }
+                return writeHistogram(histogram, min, max, numOfShades);
+
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+        }
+
+        protected bool writeHistogram(int[] histogram, int min, int max, int numOfShades)
+        {
+            try
+            {
+
+                using (StreamWriter outfile = new StreamWriter(@"C:\\dev\histogram.txt"))
+                {
+                    for(int i=0; i<histogram.Length; i++)
+                    {
+                        string line = i.ToString() + "\t" + histogram[i].ToString();
+                        outfile.WriteLine(line);
+                    }
+
+                    string minStr = "min\t" + min.ToString();
+                    outfile.WriteLine(minStr);
+
+                    string maxStr = "max\t" + max.ToString();
+                    outfile.WriteLine(maxStr);
+
+                    string numShadesStr = "num of shades\t" + numOfShades.ToString();
+                    outfile.WriteLine(numShadesStr);
+                }
+                return true;
+            }
+                catch(Exception ex)
+            {
+                return false;
+            }
+        }
+
+        protected void createBmp(FileInfo[] fileInfo)
+        {
+            // create a bmp instance -- consider 8bpp only -- will requantize 12bpp -> 8 bpp
+            Bitmap bmp = new Bitmap(fileInfo.Count(), 2048);
+
+            for (int x = 0; x < fileInfo.Count(); x++)
+            {
+                // convert 1 line of ascii to bytes
+                string[] pixels = loadAscii(fileInfo[x]);
+                for (int y = 0; y < 2048; y++)
+                {
+                    int p = requantize(pixels[y]);  // 12bpp -> 8bpp linear scaling for now
+                    Color color = Color.FromArgb(p, p, p);
+                    bmp.SetPixel(x, y, color);
+                }
+            }
+            bmp.Save("c:\\testImage\\test.bmp");
+        }
+
+        protected int requantize(string value)
+        {
+            double pixel = double.Parse(value);
+            double p = pixel / 4096 * 256;
+            return (int)(p);
+        }
+
+        protected string[] loadAscii(FileInfo fileInfo)
+        {
+            try
+            {
+                string[] lines = new string[2048];
+                int index = 0;
+                System.IO.StreamReader file = new System.IO.StreamReader(fileInfo.FullName);
+                while (index < 2048 && (lines[index] = file.ReadLine()) != null )
+                {
+                    index++;
+                }
+                return lines;
+            }
+            catch(Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
